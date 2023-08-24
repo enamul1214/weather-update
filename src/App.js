e53af750d42f4c2d8de3a1bb97a2f0e8a322fe67
enamul1214 <enamul.kabirseu@gmail.com>
2023-08-24 21:26:37 +0600

    tailwindcss configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="border-2 color-red">
       <Header />
       <Main />
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-	return <div>Header</div>;
+	return (
+		<div className="text-[50px] bg-[#1da1f2] text-white justify-center flex">
+			Header
+		</div>
+	);
 }
 
 export default Header;
